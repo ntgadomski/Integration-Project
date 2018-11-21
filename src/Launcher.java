754bc097ecc04fac5f1a52c1a356dc250bc02d22
spudnick79 <42775030+spudnick79@users.noreply.github.com>
2018-11-21 18:39:16 -0500

    PSI 2

---
+++ src/Launcher.java
@@ -0,0 +1,37 @@
+/*
+ * Created by: Nickolas Gadomski
+ * This program is a launcher that will
+ * launch the required instructions of
+ * the whole integration project.
+ */
+import java.util.Scanner; 
+
+public class Launcher {
+
+  public static Scanner kb = new Scanner(System.in);
+  public static void main(String[] args) {
+
+    int key;
+    System.out.println("HI VANSELOW!, WELCOME TO THE INTEGRATION PROJECT.");
+    System.out.println("Enter 1 for PART ONE \nEnter 2 for PART TWO");
+
+    try {
+      while(true){
+        key = kb.nextInt();
+        if(key == 1){
+         PartOne.main(args);
+        } else if (key == 2){
+         PartTwo.main(args);
+        }else{
+          System.out.println("There are only two options, TRY AGAIN");
+        }
+      } // try
+
+
+    }catch (Exception e) {
+      System.err.println("ERROR ERROR ENTRY MUST BE INT RUN PROGRAM AGAIN");
+      PartOne.main(args);
+
+    } // catch
+  }
+}
---
+++ src/SeparateFile.java
@@ -0,0 +1,59 @@
+
+public class SeparateFile{
+
+  private String firstName = null;
+  private String lastName = null;
+
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+
+  public String getLastName() {
+    return lastName;
+  }
+
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+
+  public SeparateFile(String argA, String argB ){
+
+    this.firstName = argA;
+    this.lastName = argB;
+
+
+  } // constructor
+
+  public SeparateFile(String argA){
+
+    this.firstName = argA;
+
+
+  } // overloaded constructor
+
+  public SeparateFile(){
+    this.firstName = null;
+    this.lastName = null;
+  } // no args constructor
+
+  public void printName(){
+
+    System.out.print(this.firstName + " ");
+
+    if (this.lastName != null)
+      System.out.print(lastName);
+
+  }
+
+
+}
+
